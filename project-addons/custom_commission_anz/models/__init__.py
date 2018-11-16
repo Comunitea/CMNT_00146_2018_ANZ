987583d1e8f36ccc72732aac4b7cab2218557547
javierjcf <javierjcf@gmail.com>
2018-11-16 14:00:56 +0100

    [FIX]flake8 corrections

--- project-addons/custom_commission_anz/models/__init__.py
+++ project-addons/custom_commission_anz/models/__init__.py
@@ -1,4 +1,4 @@
 # © 2018 Comunitea - Javier Colmenero <javier@comunitea.com>
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
-from . import res_partner
-from . import sale_order
+from . import res_partner  # noqa
+from . import sale_order # noqa
--- project-addons/partner_custom_anz/models/__init__.py
+++ project-addons/partner_custom_anz/models/__init__.py
@@ -2,5 +2,3 @@
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 from . import res_partner  # noqa
 from . import account_analytic  # noqa
-from . import account_invoice
-
