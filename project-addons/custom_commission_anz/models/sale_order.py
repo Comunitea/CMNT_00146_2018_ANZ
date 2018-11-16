4071b967f5c7f92df4c463a50d91e36444aa3b1c
javierjcf <javierjcf@gmail.com>
2018-11-16 13:58:18 +0100

    [ADD]custom_commission_anz

---
+++ project-addons/custom_commission_anz/models/sale_order.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+from odoo import api, fields, models
+
+
+class SaleOrderLine(models.Model):
+    _inherit = "sale.order.line"
+
+    @api.multi
+    @api.onchange('product_id')
+    def product_id_change(self):
+        res = super(SaleOrderLine, self).product_id_change()
+        partner = self.order_id.partner_id
+        brand = self.product_id.product_brand_id
+        if not brand:
+            return res
+        agent_vals = []
+        agents = partner.get_brand_agents(brand.id)
+        if not agents:
+            agents = partner.agents
+
+        for agent in partner.agents:
+            commission_id = agent.get_brand_commission(brand.id)
+            if not commission_id:
+                commission_id = agent.commission.id
+
+            values = (0, 0, {'agent': agent.id, 'commission': commission_id})
+            agent_vals.append(values)
+        self.update({'agents': agent_vals})
+        return res
