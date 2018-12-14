8b5dcba8040518859b198c8d78be14efb4957d30
Kiko Sánchez <kiko@comunitea.com>
2018-12-14 15:40:07 +0100

    [DEV] sale_order_line_tree. Pequeñas mejoras en código

--- project-addons/sale_order_line_tree/models/sale_order.py
+++ project-addons/sale_order_line_tree/models/sale_order.py
@@ -37,7 +37,7 @@ class SaleOrder(models.Model):
 
     @api.multi
     def action_view_order_lines(self):
-
+        self.ensure_one()
         model_data = self.env['ir.model.data']
         tree_view = model_data.get_object_reference(
             'sale_order_line_tree', 'sale_order_line_tree_view')
@@ -48,7 +48,7 @@ class SaleOrder(models.Model):
         action['views'] = {
             (tree_view and tree_view[1] or False, 'tree')}
 
-        action['domain'] = [('id', 'in', self.order_line.ids)]
+        action['domain'] = [('order_id', '=', [self.id])]
 
         action['context'] = {
             'default_order_id': self.id,
@@ -60,7 +60,10 @@ class SaleOrder(models.Model):
         # action['view_ids'] = [tree_view and tree_view[1]]
         action.update(
             {'tax_id': {'domain': [('type_tax_use', '=', 'sale'),
-                                   ('company_id', '=', self.company_id)]}})
+                                   ('company_id', '=', self.company_id)]}}
+             )
+
+
         return action
 
 
@@ -68,7 +71,7 @@ class SaleOrderLine(models.Model):
 
     _inherit = "sale.order.line"
 
-    product_tmpl_id = fields.Many2one('product.template', string="Template")
+    product_tmpl_id = fields.Many2one(related="product_id.product_tmpl_id", string="Template", store=True)
     variant_sequence = fields.Integer(string="Variant sequence")
 
     _order = 'sequence, variant_sequence'
@@ -78,42 +81,22 @@ class SaleOrderLine(models.Model):
         return super(SaleOrderLine, self).write(vals)
 
     @api.multi
-    @api.onchange('product_tmpl_id')
-    def product_tmpl_id_change(self):
-        self.ensure_one()
-        self.product_id = False
-        result = super(SaleOrderLine, self).product_id_change() or []
-        if self.product_tmpl_id:
-            result.update({'domain': {'product_id':
-                                      [('product_tmpl_id', '=',
-                                        self.product_tmpl_id.id)]}
-                           })
-        else:
-            result.update({'domain':[]})
-
-        return result
-
-    @api.multi
     @api.onchange('product_id')
     def product_id_change(self):
         result = super(SaleOrderLine, self).product_id_change()
         if self.product_id:
-            if not self.product_tmpl_id:
-                self.product_tmpl_id = self.product_id.product_tmpl_id
-
             self.variant_sequence = self.product_id.attribute_value_ids.\
                 sequence
         else:
             self.variant_sequence = 0
-            self.product_tmpl_id = False
         return result
 
     @api.model
     def create(self, vals):
 
-        if 'product_tmpl_id'not in vals and vals.get('product_id', False):
+        if 'product_tmpl_id' not in vals and vals.get('product_id', False) and False:
+            import ipdb; ipdb.set_trace()
             tmpl_id = self.env['product.product'].\
-                search_read([('id', '=', vals['product_id'])],
-                            ['product_tmpl_id'])[0]['product_tmpl_id'][0]
+                search_read([('id', '=', vals['product_id'])],['product_tmpl_id'])[0]
             vals.update({'product_tmpl_id': tmpl_id})
         return super(SaleOrderLine, self).create(vals)
