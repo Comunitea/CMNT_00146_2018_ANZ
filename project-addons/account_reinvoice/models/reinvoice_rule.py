67608664dcf7ffb4a656de0ef2e4cac73a53a601
Kiko Sánchez <kiko@comunitea.com>
2019-01-24 21:26:06 +0100

    [IMP] account_reinvoice, account_reinvoice_txt Mejoras en importaciond de txt y refactura

--- project-addons/account_reinvoice/models/reinvoice_rule.py
+++ project-addons/account_reinvoice/models/reinvoice_rule.py
@@ -1,6 +1,6 @@
 # Copyright 2016 Acsone SA/NV
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
-from odoo import api, fields, models
+from odoo import api, fields, models, _
 from odoo.exceptions import UserError
 
 class ReInvoiceRule(models.Model):
@@ -13,13 +13,17 @@ class ReInvoiceRule(models.Model):
     #scheduled_sale = fields.Boolean('Scheduled sale', help="If checked, apply scheduled discount, else repos discount", default=False)
     supplier_discount = fields.Float('Supplier Discount')
     customer_discount = fields.Float('Customer Discount')
-    affiliate = fields.Boolean('Affiliate', default=False)
+    affiliate = fields.Boolean('Affiliate', default=True)
     supplier_customer_ranking_id = fields.Many2one('supplier.customer.ranking', string="Clasificación")
-
-
-
+    order_type = fields.Selection([('all_discount', 'Mantener descuento'), ('0_discount', 'Descuento a 0'), ('apply_discount', 'Aplicar regla')],
+                                  string="Tipo de regla",
+                                  default='apply_discount',
+                                  help="Mantener descuento: No aplica ninguna regla\n"
+                                       "Descuento a 0: Aplica siempre 0%\n"
+                                       "Aplicar regla: Aplica el descuento de la regla que se obtenga")
 
     def get_reinvoice_discount(self, line, supplier):
+
         partner_id = line.invoice_id.partner_id
         product_id = line.product_id
         supplier_data = self.env['partner.supplier.data'].search([('customer_supplier_id', '=', line.invoice_id.associate_id.id)], limit=1)
@@ -28,17 +32,18 @@ class ReInvoiceRule(models.Model):
                   '|', ('supplier_discount', '=', 0.00), ('supplier_discount', '=', line.discount),
                   '|', ('supplier_customer_ranking_id', '=', supplier_data and supplier_data.supplier_customer_ranking_id.id or False), ('supplier_customer_ranking_id', '=', False),
                   ('supplier_id', '=', supplier.id),
-                  #('scheduled_sale', '=', scheduled_sale),
                   ('affiliate', '=', partner_id.affiliate)
                   ]
-        rule = self.search(domain, order='partner_id asc, brand_id asc, supplier_customer_ranking_id asc, supplier_discount desc')
-        print (rule.mapped('customer_discount'))
+        rule = self.search(domain, order='partner_id asc, brand_id asc, supplier_customer_ranking_id asc, supplier_discount desc', limit=1)
         if not rule:
             raise UserError(
-                _('No reinvoice rule founded for discount of %s')
+                ('No se ha encontrado una regla de refactura')
                 % line.discount)
-        res = rule[0]
-        print ('Regla {} para {} :{} '.format(res.id, res.partner_id, res.customer_discount))
-        return res.customer_discount
+        if rule.order_type == 'all_discount':
+            return line.discount, False
+        if rule.order_type == '0_discount':
+            return 0, False
+
+        return rule.customer_discount, rule.id
 
 
