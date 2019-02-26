cc94ffd7e68f05963af9af2c3c65dec070328b30
ruben <ruben@comunitea.com>
2019-02-26 12:15:25 +0100

    [ADD] Contact pages

--- project-addons/theme_anzamar/__manifest__.py
+++ project-addons/theme_anzamar/__manifest__.py
@@ -30,7 +30,7 @@
     'version': '1.0',
     'summary': 'FrontEnd customization for Anzamar Website',
     'description': '',
-    'category': 'Theme/Ecommerce',
+    'category': 'Theme',
     'author': 'Comunitea',
     'website': 'http://www.comunitea.com',
     'license': 'AGPL-3',
