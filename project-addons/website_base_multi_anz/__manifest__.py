6ed52dd6bf4bc3aaae75ab536d152193432270e8
neo-oien <i@oien.ru>
2019-02-28 16:04:32 +0100

    [UPD] Base multi-company module

--- project-addons/website_base_multi_anz/__manifest__.py
+++ project-addons/website_base_multi_anz/__manifest__.py
@@ -50,7 +50,7 @@
     ],
     'data': [
         'data/company_data.xml',
-        'data/menu_data.xml',
+        # 'data/menu_data.xml',
         'data/page_data.xml',
         'data/website_data.xml',
         'views/res_company_views.xml',
