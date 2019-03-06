eafed9153499aa7d7845b8e5e34eecab4a2904ba
neo-oien <i@oien.ru>
2019-03-06 12:19:59 +0100

    [UPD] Themes

--- project-addons/theme_feel_point/__manifest__.py
+++ project-addons/theme_feel_point/__manifest__.py
@@ -30,7 +30,7 @@
     'version': '1.0',
     'summary': 'FrontEnd customization for Feel Point Website',
     'description': '',
-    'category': 'Theme',
+    'category': 'Website',
     'author': 'Comunitea',
     'website': 'http://www.comunitea.com',
     'license': 'AGPL-3',
