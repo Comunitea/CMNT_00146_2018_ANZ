95833fe20092f4ff43001a871962fe822bba88fb
neo-oien <i@oien.ru>
2019-03-08 12:54:57 +0100

    [UPD] Forestal theme: header scroll

--- project-addons/theme_anzamar/static/js/main.js
+++ project-addons/theme_anzamar/static/js/main.js
@@ -1,12 +1,18 @@
 /* Hide top menu part with scroll */
 $(window).on('scroll', function() {
-    if ($(window).scrollTop() > 135) {
+    if ($(window).scrollTop() > 145) {
         $('.wp-contact-navbar').hide();
-        if(!$('header').hasClass('homepage-header')){$('.header-fixed-margin').show();}
+        if(!$('header').hasClass('homepage-header')){
+            $('.header-fixed-margin').show();
+            $('#wrap').css({'margin-top': '130px'});
+        }
         $('header').addClass('fixed');
     } else {
         $('header').removeClass('fixed');
-        if(!$('header').hasClass('homepage-header')){$('.header-fixed-margin').hide();}
-        $('.wp-contact-navbar').slideDown(100);
+        if(!$('header').hasClass('homepage-header')){
+            $('#wrap').css({'margin-top': '0px'});
+            $('.header-fixed-margin').hide();
+        }
+        $('.wp-contact-navbar').show();
     }
 });
