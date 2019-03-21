65dda647ba5d09aba48bd6dfa1ba650c53c0cd91
neo-oien <i@oien.ru>
2019-03-21 14:14:56 +0100

    [UPD] Point Sport: Our shops map

--- project-addons/theme_point_sport/static/js/our-shops-map.js
+++ project-addons/theme_point_sport/static/js/our-shops-map.js
@@ -1,28 +1,49 @@
 // Add national presence Google map
 function pointShopsInitMap() {
     // Set var
-    var icon1 = '/theme_anzamar/static/img/place-marker.png'
-        offset = new google.maps.Size(0, 48)
-        map_center = {lat: 40.352, lng: -4.155}
-        shop_01 = {lat: 37.0472176, lng: -4.861957}
-        shop_02 = {lat: 36.710308, lng: -4.4629974}
-        shop_03 = {lat: 36.6945063, lng: -4.4517726}
+    var icon1 = '/theme_anzamar/static/img/map/place-marker.png'
+        offset = new google.maps.Size(0, 46)
+        toclaster = []
+
     // Map create
     var map = new google.maps.Map(document.getElementById('map'), {
         zoom: 6,
-        center: map_center
+        center: {lat: 40.0, lng: -4.0}
     });
-    // Set markers content
-    var info_shop_01 = new google.maps.InfoWindow({content: '<div><h3>Deportes Palacios</h3><p>Teléfono: 952 723 495</p><p>Plaza de España, 1, Campillos, Malaga</p></div>', pixelOffset: offset})
-        info_shop_02 = new google.maps.InfoWindow({content: '<div><h3>Factory Point Sport</h3><p>Teléfono: 952 239 000</p><p>Calle Mefistofeles, 13, Málaga</p></div>', pixelOffset: offset})
-        info_shop_03 = new google.maps.InfoWindow({content: '<div><h3>Master Sport</h3><p>Teléfono: 952 239 000</p><p>Avd. De Velazquez, 71, Málaga</p></div>', pixelOffset: offset})
-    // Add markers
-    var marker_shop_01 = new google.maps.Marker({position: shop_01, map: map, icon: icon1, title: 'Deportes Palacios'})
-        marker_shop_02 = new google.maps.Marker({position: shop_02, map: map, icon: icon1, title: 'Factory Point Sport'})
-        marker_shop_03 = new google.maps.Marker({position: shop_03, map: map, icon: icon1, title: 'Master Sport'})
 
-    // Add marker click action
-    marker_shop_01.addListener('click', function() {info_shop_01.open(map, marker_shop_01);});
-    marker_shop_02.addListener('click', function() {info_shop_02.open(map, marker_shop_02);});
-    marker_shop_03.addListener('click', function() {info_shop_03.open(map, marker_shop_03);});
+    var locations = [
+        {lat: 37.0472176, lng: -4.861957, title: 'Deportes Palacios', phone: '952 723 495', address: 'Plaza de España, 1, Campillos, Malaga'},
+        {lat: 36.710308, lng: -4.4629974, title: 'Factory Point Sport', phone: '952 239 000', address: 'Calle Mefistofeles, 13, Málaga'},
+        {lat: 36.6945063, lng: -4.4517726, title: 'Master Sport', phone: '952 239 000', address: 'Avd. De Velazquez, 71, Málaga'}
+    ]
+
+    var markers = locations.map(function(location){
+        // Add marker
+        var marker = new google.maps.Marker({
+            position: location,
+            map: map,
+            icon: icon1,
+            title: location.title,
+        });
+        toclaster.push(marker)
+        // Add info popup
+        var window = new google.maps.InfoWindow({
+            content: '<div><h3>'+location.title+'</h3><p>Teléfono: '+location.phone+'</p><p>'+location.address+'</p></div>',
+            maxWidth: 250,
+            pixelOffset: offset
+        });
+        // Add marker click action
+        return marker.addListener('click', function() {window.open(map, marker);});
+    });
+
+    var markerCluster = new MarkerClusterer(map, toclaster, {
+        maxZoom: 9,
+        styles: [{
+            textColor: '#333333',
+            textSize: '16',
+            height: 53,
+            width: 53,
+            url: '/theme_anzamar/static/img/map/cluster-marker-1.png'
+        }]});
+
 }
