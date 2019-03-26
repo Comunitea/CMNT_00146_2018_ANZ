b6cb40e77f98821d81b949c7bc9479b6da17fcce
Kiko Sánchez <kiko@comunitea.com>
2019-03-26 11:52:01 +0100

    [DEV] stock_account_shared_locations. Moóulo para correcto funcioanmiento si las ubicaciones no tienen compañia

---
+++ project-addons/stock_account_shared_locations/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# © 2018 Comunitea - Kiko Sánchez <kiko@comunitea.com>
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+{
+    'name': 'Stock Move Shared Locations',
+    'version': '11.0.0.0.0',
+    'category': 'Custom',
+    'license': 'AGPL-3',
+    'author': "Comunitea, ",
+    'depends': [
+        'stock_account',
+    ],
+    'data': [
+    ],
+    'installable': True,
+}
