6559356cbd62a7038fd6a1d7961649a83741ed77
Kiko Sánchez <kiko@comunitea.com>
2019-04-01 12:48:42 +0200

    [IMP] stock_picking_custom. Verificar que las ubicaciones internas tengan siempre codigo de barras

--- project-addons/stock_picking_custom/models/stock_location.py
+++ project-addons/stock_picking_custom/models/stock_location.py
@@ -3,7 +3,7 @@
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 from odoo import _, api, models, fields
-
+from odoo.exceptions import ValidationError
 
 class StockLocation(models.Model):
 
@@ -15,10 +15,36 @@ class StockLocation(models.Model):
     def set_barcode_field(self):
         total = len(self)
         inc = 0
-        for location in self.filtered(lambda x: x.usage == 'internal' and x.location_id.ubic):
-            barcode = "{:02d}.{:02d}.{:02d}.{:02d}".format(location.location_id.ubic, location.posx, location.posy, location.posz)
+        print (self)
+        for location in self:
+            ubic = location.location_id and (location.location_id.ubic or location.location_id.id) or 0
+            barcode = "{:05d}.{:02d}.{:02d}.{:02d}".format(ubic, location.posx, location.posy, location.posz)
             location.barcode = barcode
             inc += 1
             print ('{} de {} >> {}: Codigo: {}'.format(inc, total, location.display_name, location.barcode))
 
+    @api.onchange('posx', 'posy', 'posz')
+    def onchange_act_barcode(self):
+        self.set_barcode_field()
 
+    def check_vals(self, usage, posx, posy, posz, barcode):
+        if usage=='internal' and not posx and not posy and not posz:
+            raise ValidationError('Las ubicaciones internas  los campos cliente, tarifa, compañia y tipo de venta')
+        if usage=='internal' and not barcode:
+            raise ValidationError('Las ubicaciones internas deben tener un código de barras')
+
+    @api.model
+    def create(self, vals):
+        self.check_vals(vals['usage'], vals['posx'], vals['posy'], vals['posz'], vals['barcode'])
+        return super().create(vals)
+
+    @api.multi
+    def write(self, vals):
+        res = super().write(vals)
+        if len(self) == 1:
+            self.check_vals(self.usage, self.posx, self.posy, self.posy, self.barcode)
+        if 'ubic' in vals:
+            for loc in self:
+                locs = self.env['stock.location'].search([('usage','=', 'internal'), ('location_id', '=', loc.id)])
+                locs.set_barcode_field()
+        return res
