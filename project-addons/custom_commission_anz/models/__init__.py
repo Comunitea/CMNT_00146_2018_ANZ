75e900513171307961d8ed91285183649a2af521
javierjcf <javierjcf@gmail.com>
2019-04-09 18:58:08 +0200

    [ADD]custom_commission_anz: Comisiones en facturas

--- project-addons/custom_commission_anz/models/__init__.py
+++ project-addons/custom_commission_anz/models/__init__.py
@@ -4,5 +4,6 @@ from . import agent_commission_brand  # noqa
 from . import res_partner  # noqa
 from . import sale_commission_mixin # noqa
 from . import sale_order # noqa
+from . import account_invoice # noqa
 from . import settlement # noqa
 from . import product_brand # noqa
