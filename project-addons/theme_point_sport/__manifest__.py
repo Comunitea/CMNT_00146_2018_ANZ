d05ebf9f6f853137ffa0ae52f2778a5f6dc5ef22
ruben <ruben@comunitea.com>
2019-04-30 19:38:15 +0200

    [ADD] New search features

--- project-addons/theme_point_sport/__manifest__.py
+++ project-addons/theme_point_sport/__manifest__.py
@@ -46,6 +46,7 @@
         'multi_company_base',
         'website_base_multi_anz',
         'website_sale_hide_price',
+        'base_search_fuzzy',
     ],
     'data': [
         'data/theme_data.xml',
--- project-addons/website_base_multi_anz/controllers/website.py
+++ project-addons/website_base_multi_anz/controllers/website.py
@@ -2,11 +2,40 @@
 
 import json
 from odoo import http, _
+from odoo.osv import expression
 from odoo.http import request
 from odoo.addons.website_sale.controllers.main import WebsiteSale
 
 
-class MultiUpdateCart(WebsiteSale):
+class WebsiteSaleExtended(WebsiteSale):
+
+    def _get_search_domain(self, search, category, attrib_values):
+        domain = super(WebsiteSaleExtended, self)._get_search_domain(search, category, attrib_values)
+        domain = expression.normalize_domain(domain)
+
+        if search:
+            domain_search = []
+            for srch in search.split(" "):
+                domain_search += ['|', '|', '|',
+                                  ('product_variant_ids.attribute_value_ids', 'ilike', srch),
+                                  ('public_categ_ids', 'ilike', srch),
+                                  ('product_variant_ids', 'ilike', srch),
+                                  ('product_variant_ids.product_brand_id', 'ilike', srch)]
+            domain = expression.OR([domain, domain_search])
+
+        if category:
+            categories = request.env['product.public.category']
+            # Search sub-categories of first and second depth level
+            sub_cat_l1 = categories.sudo().search([('parent_id', '=', int(category))], order='sequence')
+            sub_cat_l2 = categories.sudo().search([('parent_id', 'in', sub_cat_l1.ids)], order='sequence')
+            # Create new list of categories to show
+            list_cat = [int(category)]
+            list_cat.extend(sub_cat_l1.ids)
+            list_cat.extend(sub_cat_l2.ids)
+            # Search products from sub-categories of first and second depth level
+            domain += [('public_categ_ids', 'in', list_cat)]
+
+        return domain
 
     @http.route(['/shop/cart/multi_update'], type='json', auth="public", methods=['POST'], website=True)
     def multi_update_cart(self, update_data, product_template):
