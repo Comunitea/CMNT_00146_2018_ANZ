232dd981905721a3fa41e69692505e917652a1ce
javierjcf <javierjcf@gmail.com>
2019-05-08 10:05:38 +0200

    [FIX]website_base_multi_anz: Correcion si no vienen el equipo de venta

--- project-addons/website_base_multi_anz/models/sale_order.py
+++ project-addons/website_base_multi_anz/models/sale_order.py
@@ -9,12 +9,14 @@ class SaleOrder(models.Model):
 
     @api.model
     def create(self, vals):
-        order_type = request.env['crm.team'].sudo().search([('id', '=', vals['team_id'])]).team_type
-        if order_type == 'website':
-            website = request.env['website'].get_current_website()
-            type_id = website.sale_type_id
-            if type_id:
-                vals.update({
-                    'type_id': type_id.id
-                })
+        if vals.get('team_id', False):
+            order_type = request.env['crm.team'].sudo().search(
+                [('id', '=', vals['team_id'])]).team_type
+            if order_type == 'website':
+                website = request.env['website'].get_current_website()
+                type_id = website.sale_type_id
+                if type_id:
+                    vals.update({
+                        'type_id': type_id.id
+                    })
         return super(SaleOrder, self).create(vals)
