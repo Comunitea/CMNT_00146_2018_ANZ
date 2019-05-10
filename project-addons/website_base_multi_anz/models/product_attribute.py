e80e5abb1797192e196e1dc0a91758240e394c74
JohnnyVM <juan.vazquez@anzamar.com>
2019-05-10 11:33:11 +0200

    product_attribute webiste_publishe by default to False

--- project-addons/website_base_multi_anz/models/product_attribute.py
+++ project-addons/website_base_multi_anz/models/product_attribute.py
@@ -6,4 +6,4 @@ from odoo import api, fields, models
 class ProductAttribute(models.Model):
     _inherit = 'product.attribute'
 
-    website_published = fields.Boolean(string='Published', default=True)
+    website_published = fields.Boolean(string='Published', default=False)
