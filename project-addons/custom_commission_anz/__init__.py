20e765760d2d14f7e1d6e84bf4eacd72b86497f7
javierjcf <javierjcf@gmail.com>
2019-07-08 12:39:20 +0200

    [FIX]custom_commission_anz: Recalcular comisiones despues de refacturar

--- project-addons/custom_commission_anz/__init__.py
+++ project-addons/custom_commission_anz/__init__.py
@@ -1,3 +1,4 @@
 # © 2018 Comunitea - Javier Colmenero <javier@comunitea.com>
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 from . import models  # noqa
+from . import wizard  # noqa
