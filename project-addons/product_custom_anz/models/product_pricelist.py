c29f85e10a66050307d68143bf3a68343f54daba
javierjcf <javierjcf@gmail.com>
2019-08-13 18:17:12 +0200

    [FIX]import tools

--- project-addons/product_custom_anz/models/product_pricelist.py
+++ project-addons/product_custom_anz/models/product_pricelist.py
@@ -1,6 +1,6 @@
 # © 2016 Comunitea - Javier Colmenero <javier@comunitea.com>
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
-from odoo import fields, models, api, _
+from odoo import fields, models, api, _, tools
 from itertools import chain
 
 
