5eae005dae950cf34d049bfa283e37f447469e50
javierjcf <javierjcf@gmail.com>
2019-09-18 10:15:05 +0200

    [FIX]product_custom_label_anz: Añadir impuestos al precio etiqueta

--- project-addons/product_custom_label_anz/wizard/product_label_pricelist.py
+++ project-addons/product_custom_label_anz/wizard/product_label_pricelist.py
@@ -27,7 +27,7 @@ class ProductLabelPricelist(models.TransientModel):
     altura = fields.Integer(default=110)
     anchura = fields.Integer(default=350)
     baseincrement = fields.Integer('% añadido al precio base: ', default = 0)
-    taxes = fields.Boolean('Add taxes? (NO FUNCIONA;NO USAR)', default=True)
+    taxes = fields.Boolean('Add taxes', default=True)
 
     @api.multi
     def print_report(self):
@@ -36,20 +36,35 @@ class ProductLabelPricelist(models.TransientModel):
         ctx = self._context.copy()
         if self.price_list:
             ctx.update(pricelist=self.price_list.id)
-        res = self.with_context(ctx).env['product.product'].browse(self._context.get('active_ids', []))
-        p_ids=[]
+        res = self.with_context(ctx).env['product.product'].\
+            browse(self._context.get('active_ids', []))
+        p_ids = []
         h = self.altura
         if self.discount:
             rows = 4
         else:
             rows = 3
         for p in res:
+            price = p.price
+            lst_price = p.lst_price if not self.discount else \
+                p.lst_price * (1 + self.baseincrement/100)
+            if self.taxes:
+                company = self.env['res.company']._company_default_get()
+                price_tax = p.taxes_id.compute_all(
+                    price, company.currency_id, 1, product=p)
+                price_lst_tax = p.taxes_id.compute_all(
+                    price, company.currency_id, 1, product=p)
+                price = price_tax['total_included']
+                lst_price = price_lst_tax['total_included']
+
             vals = {
-                'name': p.product_tmpl_id.ref_template or p.product_tmpl_id.default_code,
+                'name': p.product_tmpl_id.ref_template or
+                p.product_tmpl_id.default_code,
                 'barcode': p.barcode,
-                'attr_name': p.attribute_value_ids and p.attribute_value_ids[0].name or 'Única',
-                'price': p.price,
-                'lst_price': p.lst_price if not self.discount else p.lst_price * (1 + self.baseincrement/100),
+                'attr_name': p.attribute_value_ids and
+                p.attribute_value_ids[0].name or 'Única',
+                'price': price,
+                'lst_price': lst_price,
                 'height_r': h / rows,
                 'height_t': h,
                 'discount': self.discount,
@@ -60,4 +75,6 @@ class ProductLabelPricelist(models.TransientModel):
             p_id = ppl.create(vals).id
             p_ids.append(p_id)
 
-        return self.env.ref('product_custom_label_anz.action_print_label_docs_pricelist').report_action(ppl.browse(p_ids))
+        return self.env.ref(
+            'product_custom_label_anz.action_print_label_docs_pricelist').\
+            report_action(ppl.browse(p_ids))
