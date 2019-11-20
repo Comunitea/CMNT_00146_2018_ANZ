baecb394ab71167daa3806954b356b23fe7ea9f1
javierjcf <javierjcf@gmail.com>
2019-11-20 12:01:24 +0100

    [FIX]sale_custom_anz: Mostrar stock ern el grid

--- project-addons/sale_custom_anz/models/product_attribute.py
+++ project-addons/sale_custom_anz/models/product_attribute.py
@@ -25,7 +25,8 @@ class ProductAttributeValue(models.Model):
 
             # Solo si en el producto hay una línea de atributos
             if len(tmp_obj.attribute_line_ids.filtered(
-                    lambda x: x.attribute_id.create_variant is True)) == 1:
+                    lambda x: x.attribute_id.create_variant is True and
+                    x.attribute_id.feature is False)) == 1:
                 p_ids = tmp_obj.product_variant_ids
                 res = []
                 qty_field = 'qty_available'
@@ -40,7 +41,10 @@ class ProductAttributeValue(models.Model):
 
                 # Añado el campo stock en al nombre del atributo
                 for product in p_ids.filtered(lambda x: x.attribute_value_ids):
-                    a_value = product.attribute_value_ids[0]
+                    a_value = product.attribute_value_ids.filtered(
+                        lambda v: v.attribute_id.create_variant and
+                        v.attribute_id.feature is False
+                    )
                     res.append([a_value.id, "%s (%s)" %
                                 (a_value.name, product[qty_field])])
                 return res
