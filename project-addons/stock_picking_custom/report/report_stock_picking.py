7bd93326d5651ac06ea43c3849b8abff96cf8867
Kiko Sánchez <kiko@comunitea.com>
2019-11-20 13:10:48 +0100

    [IMP] stock_picking_custom Albarán de cliente

--- project-addons/stock_picking_custom/report/report_stock_picking.py
+++ project-addons/stock_picking_custom/report/report_stock_picking.py
@@ -20,7 +20,7 @@ class ShippingCustomCustomerAnz(models.AbstractModel):
             #sorted_lines = doc_id.move_line_ids.sorted(key=lambda m: (m.location_id.sequence + m.product_id.attribute_value_ids.sequence/1000))
             sorted_lines = doc_id.move_line_ids.sorted(key=lambda m: (
             m.location_id.sequence, m.product_id.product_tmpl_id, m.product_id.attribute_value_ids.sequence))
-        
+
         template_qty = {}
         for move in sorted_lines:
             template = move.product_id.product_tmpl_id
@@ -31,6 +31,44 @@ class ShippingCustomCustomerAnz(models.AbstractModel):
             else:
                 template_qty.update({str: qty})
 
+
+        docargs = {
+           #'group_by_template': False,
+           'doc_ids': docids,
+           'doc_model': '',
+           'docs': doc_id,
+           'sorted_lines': sorted_lines,
+           'template_qty': template_qty
+        }
+        return docargs
+
+
+
+class ShippingCustomCustomerAnzGroupTemplate(models.AbstractModel):
+    _name = 'report.stock_picking_custom.report_shipping_custom_customer_anz'
+
+    def get_report_values(self, docids, data=None):
+
+        model = 'stock.picking'
+        doc_id = self.env[model].browse(docids)
+        code = doc_id.picking_type_id.code
+        if code == 'incoming':
+            #sorted_lines = doc_id.move_line_ids.sorted(key=lambda m: (m.location_dest_id.sequence + m.product_id.attribute_value_ids.sequence/1000))
+            sorted_lines = doc_id.move_line_ids.sorted(key=lambda m: (m.location_dest_id.sequence, m.product_id.product_tmpl_id, m.product_id.attribute_value_ids.sequence))
+        else:
+            #sorted_lines = doc_id.move_line_ids.sorted(key=lambda m: (m.location_id.sequence + m.product_id.attribute_value_ids.sequence/1000))
+            sorted_lines = doc_id.move_line_ids.sorted(key=lambda m: (
+            m.location_id.sequence, m.product_id.product_tmpl_id, m.product_id.attribute_value_ids.sequence))
+
+        template_qty = {}
+        for move in sorted_lines:
+            template = move.product_id.product_tmpl_id
+            str = template.name + move.location_id.name
+            qty = move.qty_done if move.state == 'done' else move.product_uom_qty
+            if str in template_qty.keys():
+                template_qty[str] += qty
+            else:
+                template_qty.update({str: qty})
 
         docargs = {
            #'group_by_template': False,
