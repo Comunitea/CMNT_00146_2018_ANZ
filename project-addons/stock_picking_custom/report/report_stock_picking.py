255f4b7a472238e8ec89e97dbf7ab530a863de5b
Kiko Sánchez <kiko@comunitea.com>
2019-11-20 16:38:56 +0100

    [IMP] stock_picking_custom Albarán de cliente

--- project-addons/stock_picking_custom/report/report_stock_picking.py
+++ project-addons/stock_picking_custom/report/report_stock_picking.py
@@ -51,13 +51,9 @@ class ShippingCustomCustomerAnzGroupTemplate(models.AbstractModel):
         model = 'stock.picking'
         doc_id = self.env[model].browse(docids)
         code = doc_id.picking_type_id.code
-        if code == 'incoming':
-            #sorted_lines = doc_id.move_line_ids.sorted(key=lambda m: (m.location_dest_id.sequence + m.product_id.attribute_value_ids.sequence/1000))
-            sorted_lines = doc_id.move_line_ids.sorted(key=lambda m: (m.location_dest_id.sequence, m.product_id.product_tmpl_id, m.product_id.attribute_value_ids.sequence))
-        else:
-            #sorted_lines = doc_id.move_line_ids.sorted(key=lambda m: (m.location_id.sequence + m.product_id.attribute_value_ids.sequence/1000))
-            sorted_lines = doc_id.move_line_ids.sorted(key=lambda m: (
-            m.location_id.sequence, m.product_id.product_tmpl_id, m.product_id.attribute_value_ids.sequence))
+
+        sorted_lines = doc_id.move_line_ids.sorted(key=lambda m: (
+        m.location_dest_id.sequence, m.product_id.product_tmpl_id, m.product_id.attribute_value_ids.sequence))
         template_qty = {}
         for move in sorted_lines:
             template = move.product_id.product_tmpl_id
