6e54d161120fc08557571b739c10361b5b74420b
vicentecom <vicente@comunitea.com>
2020-01-15 17:33:11 +0100

    [IMP] website_base_multi_anz: Cambio para que se calcule el type_id del sale.order al seleccionar el team_id. Cambio para que el type_id sólo se calcule en el create al crearse el sale.order desde front-end

--- project-addons/website_base_multi_anz/models/sale_order.py
+++ project-addons/website_base_multi_anz/models/sale_order.py
@@ -9,15 +9,15 @@ class SaleOrder(models.Model):
 
     @api.model
     def create(self, vals):
+        ctx = self.env.context.copy()
+        from_website = ctx.get('from_website', False)
 
-        team_id = vals.get('team_id', False)
-        domain = []
-        if team_id:
-            domain = [('id', '=', team_id)]
-        order_type = request.env['crm.team'].sudo().search(domain, limit=1).team_type
-        if order_type == 'website':
-            website = request.env['website'].get_current_website()
-            type_id = website.sale_type_id
+        if from_website:
+            team_id = vals.get('team_id', False)
+            domain = []
+            if team_id:
+                domain = [('id', '=', team_id)]
+            type_id = self.recalculate_type_id(domain)
             if type_id:
                 vals.update({
                     'type_id': type_id.id
@@ -25,3 +25,16 @@ class SaleOrder(models.Model):
 
         return super(SaleOrder, self).create(vals)
 
+    @api.onchange('team_id')
+    def onchange_team_id(self):
+        if self.team_id:
+            domain = [('id', '=', self.team_id.id)]
+            type_id = self.recalculate_type_id(domain)
+            if type_id:
+                self.type_id = type_id.id
+
+    def recalculate_type_id(self, domain):
+        order_type = request.env['crm.team'].sudo().search(domain, limit=1).team_type
+        if order_type == 'website':
+            website = request.env['website'].get_current_website()
+            return website.sale_type_id
