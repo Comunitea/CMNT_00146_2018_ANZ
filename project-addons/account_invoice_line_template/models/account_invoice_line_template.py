3870233b277d1d7089d99e527a449d0d6423548f
Kiko Sánchez <kiko@comunitea.com>
2021-02-10 13:05:22 +0100

    [FIX] account_invoice_line_template/ Lista de variantes

--- project-addons/account_invoice_line_template/models/account_invoice_line_template.py
+++ project-addons/account_invoice_line_template/models/account_invoice_line_template.py
@@ -118,13 +118,14 @@ l.ref_change
     def get_qties(self):
         lines = self._get_invoice_lines()
         if len(lines) == 1:
-            return [formatLang(self.env, lines.quantity)]
+            return ['%d' % lines.product_uom_qty]
         qties_list = []
         for line in lines:
-            qty_str = ''
-            att_tag = line.product_id.attribute_value_ids
+            att_tag = line.product_id.attribute_value_ids.filtered(lambda x: x.attribute_id.main)
             if att_tag:
-                qty_str = att_tag.name_get()[0][1]
-            qty_str += ' - ' + formatLang(self.env, line.quantity)
+                qty_str = '%s - %d' % (att_tag[0].name, line.product_uom_qty)
+            else:
+                qty_str = '%d' % line.product_uom_qty
+                # qty_str += ' - ' + formatLang(self.env, line.product_uom_qty)
             qties_list.append(qty_str)
         return qties_list
