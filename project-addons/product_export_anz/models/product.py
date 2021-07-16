8794fd85d7cb3dbcf09e2b6ce05ec046aed08378
Kiko Sánchez <kiko@comunitea.com>
2021-07-16 17:05:45 +0200

    product_export_anz. Campos nuevos para exportación

--- project-addons/product_export_anz/models/product.py
+++ project-addons/product_export_anz/models/product.py
@@ -6,11 +6,12 @@ class ProductProduct(models.Model):
     _inherit = 'product.product'
 
     @api.multi
-    @api.depends('categ_id', 'attribute_value_ids' , 'attribute_value_ids.attribute_id', 'attribute_value_ids.name')
+    #@api.depends('categ_id', 'attribute_value_ids' , 'attribute_value_ids.attribute_id', 'attribute_value_ids.name')
     def _get_export_str(self):
         # Modo test solo los primaros 100
         
         for product in self:
+            print ("Artículo %s" % product.default_code)
             export_str_categ = product.categ_id.display_name
             attrib_ids = product.attribute_value_ids.filtered(lambda x: x.active)
             names = attrib_ids.filtered(lambda x: not x.attribute_id.main).sorted(lambda x:x.attribute_id.is_color).mapped('name')
@@ -27,7 +28,8 @@ class ProductProduct(models.Model):
             product.export_str_color = export_str_color
             product.export_str_value_talla = export_str_value_talla
             product.export_str_attrib_str = export_str_attrib_str
-
+            print (">>>OK")
+    """
     export_str_categ = fields.Char("Categoría (str)", compute=_get_export_str, store=True)
     export_str_image_url = fields.Char("Image URL (str)", compute=_get_export_str, store=True)
     export_str_atrib_talla = fields.Char("Tipo talla (str)", compute=_get_export_str, store=True)
@@ -35,4 +37,11 @@ class ProductProduct(models.Model):
     export_str_value_talla = fields.Char("Talla", compute=_get_export_str, store=True)
     export_str_attrib_str = fields.Char("Atributos", compute=_get_export_str, store=True)
     export_str_meta_keywords = fields.Char(related="product_meta_keywords", string="Meta Keywords (str)")
-
+    """
+    export_str_categ = fields.Char("Categoría (str)")
+    export_str_image_url = fields.Char("Image URL (str)")
+    export_str_atrib_talla = fields.Char("Tipo talla (str)")
+    export_str_color = fields.Char("Color (str)")
+    export_str_value_talla = fields.Char("Talla")
+    export_str_attrib_str = fields.Char("Atributos")
+    export_str_meta_keywords = fields.Char(related="product_meta_keywords", string="Meta Keywords (str)")
